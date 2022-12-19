dd104cb43f454df763625491cbd225637ec25789
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2022-12-19 19:06:28 -0300

    Sorteador

--- app/src/main/java/com/harvo/meuaoo/MainActivity.java
+++ app/src/main/java/com/harvo/meuaoo/MainActivity.java
@@ -3,6 +3,10 @@ package com.harvo.meuaoo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onAlterarTexto(View view){
+
+        TextView texto = findViewById(R.id.textoExibicao);
+        texto.setText("Karol indiukov");
+    }
+    public void onSortearNumero(View view){
+        TextView texto = findViewById(R.id.textoResultado);
+        int x = new Random().nextInt(11);
+
+        texto.setText("Número: "+x);
+    }
+
 }
